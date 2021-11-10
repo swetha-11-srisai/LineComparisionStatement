784cb2195504d38d0f25228c23955bb956419a87
swetha-11-srisai <swethachowdary857@gmail.com>
2021-11-10 12:39:39 -0800

    [Swetha] add : calculate distance points

---
+++ LineComparision/LineComparision/Distance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparision
+{
+    class Distance
+    {
+        double xOnePoint, xTwoPoint, yOnePoint, yTwoPoint;
+        public Distance(double xOnePoint,double xTwoPoint,double yOnePoint,double yTwoPoint)
+        {
+            this.xOnePoint = xOnePoint;
+            this.xTwoPoint = xTwoPoint;
+            this.yOnePoint = yOnePoint;
+            this.yTwoPoint = yTwoPoint;
+        }
+        public double CalculateLength()
+        {
+            double result = Math.Sqrt(Math.Pow(this.xTwoPoint - this.xOnePoint, 2)) + Math.Sqrt(Math.Pow(this.yTwoPoint - this.yOnePoint, 2));
+            return result;
+        }
+    }
+}
---
+++ LineComparision/LineComparision/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LineComparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to the line comparision ");
+            Distance point = new Distance(9.0,2.0,7.0,3.0);
+            point.CalculateLength();
+            Console.ReadLine();
+        }
+    }
+}
